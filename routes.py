f5808883f31df1fdc105d224a822906785f0802b
Megan Janickovic <megjanickovic@protonmail.com>
2019-10-25 12:09:12 -0500

    Tags
    Can add and delete tags. Can add a pronunciation to a word. Cleaned up menus and navigation.

--- routes.py
+++ routes.py
@@ -4,6 +4,8 @@ from pymysql.cursors import DictCursor
 
 app = Flask(__name__)
 
+###################### CONNECT TO DATABASE ######################
+
 mysql = MySQL(cursorclass=DictCursor)
 app.config['MYSQL_DATABASE_USER'] = ''
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
@@ -14,6 +16,8 @@ mysql.init_app(app)
 conn = mysql.connect()
 cursor = conn.cursor()
 
+###################### ROUTES ######################
+
 @app.route('/')
 def index():
     return render_template('home.html')
@@ -22,12 +26,18 @@ def index():
 def about():
     return render_template('about.html')
 
+###################### LANGUAGES #####################
+
 @app.route('/languages')
 def languages():
-    query = "SELECT id, language FROM languages"
+    query = "SELECT language_id, language FROM languages"
     cursor.execute(query)
     languages_data = cursor.fetchall()
     return render_template('languages.html', languages_data=languages_data)
+
+@app.route('/languages/<int:language>')
+def language(language):
+    return render_template('language.html')
 
 @app.route('/addLanguage', methods=['POST'])
 def addLanguage():
@@ -39,62 +49,121 @@ def addLanguage():
         conn.commit()
         return json.dumps({'html':'Language added.'})
     else:
-        return json.dumps({'html':'<span>Enter a language.</span>'})
+        return json.dumps({'html':'An error has occurred (/addLanguage)'})
 
-@app.route('/languages/<int:language>/')
-def language(language):
-    query = "SELECT language FROM languages WHERE id=%s"
-    params = language
-    cursor.execute(query, params)
-    language_data = cursor.fetchone()
-    return render_template('language.html', language=language, language_data=language_data)
+@app.route('/deleteLanguage', methods=['POST'])
+def deleteLanguage():
+    _language_id = request.form['language_id']
+    if _language_id:
+        query = "DELETE FROM languages WHERE language_id=%s"
+        params = _language_id
+        cursor.execute(query, params)
+        conn.commit()
+        return json.dumps({'html':'Language deleted.'})
+    else:
+        return json.dumps({'html':'An error has occurred (/deleteLanguage)'})
 
-@app.route('/languages/<int:language>/words')
-def words(language):
-    query = "SELECT id, word FROM words WHERE language_id=%s"
-    params = language
+##################### WORDS #####################
+
+@app.route('/languages/<int:language_id>/words')
+def words(language_id):
+    query = "SELECT word_id, word FROM words WHERE language_id=%s ORDER BY word"
+    params = language_id
     cursor.execute(query, params)
     words_data = cursor.fetchall()
-    return render_template('words.html', language=language, words_data=words_data)
+    return render_template('words.html', language_id=language_id, words_data=words_data)
+
+@app.route('/languages/<int:language_id>/words/<int:word_id>')
+def word(language_id, word_id):
+    query = "SELECT word_id, word, pronunciation, definition FROM words WHERE language_id=%s AND word_id=%s"
+    params = language_id, word_id
+    cursor.execute(query, params)
+    word_data = cursor.fetchone()
+    # need to get tags as well
+    query = "SELECT tag_id, tag FROM tags WHERE language_id=%s"
+    params = language_id
+    cursor.execute(query, params)
+    tags = cursor.fetchall()
+    return render_template('word.html', language_id=language_id, word_data=word_data, tags=tags)
 
 @app.route('/addWord', methods=['POST'])
 def addWord():
     _language_id = request.form['language_id']
     _word = request.form['word']
+    _pronunciation = request.form['pronunciation'] if request.form['pronunciation'] else None
     _definition = request.form['definition']
     if _language_id and _word and _definition:
-        query = "INSERT INTO words(language_id, word, definition) VALUES (%s, %s, %s)"
-        params = _language_id, _word, _definition
+        query = "INSERT INTO words(language_id, word, pronunciation, definition) VALUES (%s, %s, %s, %s)"
+        params = _language_id, _word, _pronunciation, _definition
         cursor.execute(query, params)
         conn.commit()
         return json.dumps({'html':'Word added.'})
     else:
-        return json.dumps({'html':'<span>Enter a word and definition.</span>'})
+        return json.dumps({'html':'An error has occurred (/addWord)'})
 
-@app.route('/languages/<int:language>/words/<int:word>')
-def word(language, word):
-    query = "SELECT word, definition FROM words WHERE language_id=%s AND id=%s"
-    params = language, word
-    cursor.execute(query, params)
-    word_data = cursor.fetchone()
-    query = "SELECT tag FROM tags, word_tag WHERE word_tag.w=%s AND tags.id=word_tag.t"
-    params = word
-    cursor.execute(query, params)
-    tags = cursor.fetchall()
-    return render_template('word.html', language=language, word=word, word_data=word_data, tags=tags)
-
-@app.route('/editDefinition', methods=['POST'])
-def editDefinition():
-    _id = request.form['id']
+@app.route('/editWord', methods=['POST'])
+def editWord():
+    _word = request.form['word']
+    _pronunciation = request.form['pronunciation'] if request.form['pronunciation'] else None
     _definition = request.form['definition']
-    if _id and _definition:
-        query = "UPDATE words SET definition=%s WHERE id=%s"
-        params = _definition, _id
+    _word_id = request.form['word_id']
+    if _word and _definition and _word_id:
+        query = "UPDATE words SET word=%s, pronunciation=%s, definition=%s WHERE word_id=%s"
+        params = _word, _pronunciation, _definition, _word_id
         cursor.execute(query, params)
         conn.commit()
-        return json.dumps({'html':'Definition updated.'})
+        return json.dumps({'html':'Word updated.'})
     else:
-        return json.dumps({'html':'<span>Enter a definition.</span>'})
+        return json.dumps({'html':'An error has occurred (/editWord)'})
+
+@app.route('/deleteWord', methods=['POST'])
+def deleteWord():
+    _word_id = request.form['word_id']
+    if _word_id:
+        query = "DELETE FROM words WHERE word_id=%s"
+        params = _word_id
+        cursor.execute(query, params)
+        conn.commit()
+        return json.dumps({'html':'Word deleted.'})
+    else:
+        return json.dumps({'html':'An error has occurred (/deleteWord)'})
+
+##################### TAGS #####################
+
+@app.route('/languages/<int:language_id>/tags')
+def tags(language_id):
+    query = "SELECT tag_id, tag FROM tags WHERE language_id=%s ORDER BY tag"
+    params = language_id
+    cursor.execute(query, params)
+    tags_data = cursor.fetchall()
+    return render_template('tags.html', language_id=language_id, tags_data=tags_data)
+
+@app.route('/addTag', methods=['POST'])
+def addTag():
+    _language_id = request.form['language_id']
+    _tag = request.form['tag']
+    if _language_id and _tag:
+        query = "INSERT INTO tags(language_id, tag) VALUES (%s, %s)"
+        params = _language_id, _tag
+        cursor.execute(query, params)
+        conn.commit()
+        return json.dumps({'html':'Tag added.'})
+    else:
+        return json.dumps({'html':'An error has occurred (/addTag)'})
+
+@app.route('/deleteTag', methods=['POST'])
+def deleteTag():
+    _tag_id = request.form['tag_id']
+    if _tag_id:
+        query = "DELETE FROM tags WHERE tag_id=%s"
+        params = _tag_id
+        cursor.execute(query, params)
+        conn.commit()
+        return json.dumps({'html':'Tag deleted.'})
+    else:
+        return json.dumps({'html':'An error has occurred (/deleteTag)'})
+
+###################### END ROUTES ######################
 
 if __name__ == "__main__":
     app.run()
